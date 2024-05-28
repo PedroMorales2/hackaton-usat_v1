c8ac1da97479492412f56fc23be84a96d7e0caae
PedroMorales2 <72403333@usat.pe>
2024-05-28 11:36:01 -0500

    PEDRO

--- controladores/docentes.py
+++ controladores/docentes.py
@@ -7,8 +7,8 @@ def listar():
         cursor.execute('SELECT * FROM docentes')
         return cursor.fetchall()
     
-def insertarDocente(nombre, apellido, correo, dedicacion,telefono, hora_asesoria):
+def insertarDocente(semestre_id,nombre, apellido, correo, dedicacion,telefono, hora_asesoria):
     with con.cursor() as cursor:
-        cursor.execute('INSERT INTO docentes (nombre, apellido, correo, dedicacion, telefono, hora_asesoria) VALUES (%s, %s, %s, %s, %s, %s)', (nombre, apellido, correo, dedicacion, telefono, hora_asesoria))
+        cursor.execute('INSERT INTO docentes (semestre_id,nombre, apellido, correo, dedicacion, telefono, hora_asesoria) VALUES (%s,%s, %s, %s, %s, %s, %s)', (semestre_id,nombre, apellido, correo, dedicacion, telefono, hora_asesoria))
         con.commit()
         
